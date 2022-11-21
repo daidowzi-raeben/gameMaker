aba9a18f2d939f3f38152d7fc297fdefa2ea9c9f
장영욱 <raeben@naver.com>
2022-11-21 17:28:17 +0900

    fix

--- pages/simulation-maker/_id.vue
+++ pages/simulation-maker/_id.vue
@@ -1,56 +1,6 @@
 <template>
   <div id="gameMaker">
     <CutInsert />
-    <div v-if="false" class="d-flex maker">
-      <div class="mobileDevice">
-        <div class="mobileDevice-warp">
-          <!-- 디바이스 미리보기 -->
-        </div>
-      </div>
-      <div class="scene d-flex">
-        <!-- 씬 -->
-        <div class="scene-scenario box">
-          <div
-            v-for="(v, i) in SCENE"
-            :key="i"
-            class="item"
-            @click="onClickScenario"
-          >
-            {{ v.title }} - {{ v.subTitle }}
-          </div>
-          <div>
-            <div v-if="isBtn === false" @click="isBtn = true">
-              <button>추가 +</button>
-            </div>
-            <div v-else>
-              <div>
-                <input v-model="insert.title" placeholder="제목 ex) 제 1장" />
-              </div>
-              <div>
-                <input2270
-                  v-model="insert.subTitle"
-                  placeholder="부제 ex) 흔들리는 꽃들 속에서"
-                />
-              </div>
-              <div>
-                <button @click="isBtn = false">저장</button>
-                <button @click="isBtn = false">X</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="scene-round box">
-          <div v-for="(v, i) in SCENE" :key="i" class="item">
-            {{ v.title }} - {{ v.subTitle }}
-          </div>
-        </div>
-        <div>
-          <!-- 이벤트 -->
-        </div>
-      </div>
-      <div class="">asd</div>
-    </div>
-    <div></div>
   </div>
 </template>
 
@@ -72,7 +22,9 @@ export default {
   },
 
   data() {
-    return { params: {}, insert: {}, isBtn: false }
+    return {
+      params: {},
+    }
   },
   computed: {
     ...mapState(['LOGIN', 'LOADING', 'SCENE']),
@@ -87,40 +39,8 @@ export default {
   methods: {
     ...mapActions(['ACTION_AXIOS_GET']),
     ...mapMutations(['']),
-
-    onChangeScene({ target }) {
-      console.log(target.value)
-    },
-    onClickScenario() {},
   },
 }
 </script>
 
-<style lang="scss">
-input {
-  border: 1px solid #ddd;
-}
-#gameMaker {
-  min-width: 1200px;
-  width: 1200px;
-  margin: 0 auto;
-}
-.mobileDevice {
-  width: 360px;
-  height: 640px;
-  border: 2px solid #ccc;
-  border-radius: 20px;
-  overflow: hidden;
-  &-warp {
-  }
-}
-.scene {
-  margin-left: 10px;
-  .box {
-    border: 1px solid #ccc;
-    margin-right: 10px;
-    width: 200px;
-  }
-}
-/* 360 * 640 */
-</style>
+<style lang="scss"></style>
