3b7ec04aa3bd650e101678fd88d69e99775371f4
장영욱 <a02394@hanatour.com>
2022-11-28 22:57:56 +0900

    fix

--- plugins/AwesomeSwiper.js
+++ plugins/AwesomeSwiper.js
@@ -1,18 +1,3 @@
-// import Vue from 'vue'
-// import VueAwesomeSwiper from 'vue-awesome-swiper'
-// import 'swiper/dist/css/swiper.css'
-// Vue.use(VueAwesomeSwiper)
-
-import createApp from 'vue'
-import SwiperClass, { /* swiper modules... */ } from 'swiper'
+import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-
-// import swiper module styles
-import 'swiper/dist/css/swiper.css'
-// more module style...
-
-// use swiper modules
-SwiperClass.use([/* swiper modules... */])
-
-const app = createApp()
-app.use(VueAwesomeSwiper)
+Vue.use(VueAwesomeSwiper)
