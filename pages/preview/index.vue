9693ee028cd403ada78165b8b6c42b63a5096087
장영욱 <raeben@naver.com>
2022-12-14 15:09:07 +0900

    fix

--- pages/preview/index.vue
+++ pages/preview/index.vue
@@ -8,8 +8,16 @@
           <img :src="inApp.cr" alt="" class="character right" />
         </div>
         <div class="dialogue">
-          <span class="name">{{ inApp.crName }}</span>
-          <p class="text">{{ inApp.text }}</p>
+          <span v-if="cutType === 1" class="name">{{ inApp.crName }}</span>
+          <p v-if="cutType === 1" class="text">{{ inApp.text }}</p>
+          <p v-if="cutType === 2" class="text">{{ inApp.narration }}</p>
+          <p v-if="cutType === 3" class="text">
+            {{ inApp.questions.text[0] }}<br />{{ inApp.questions.text[1]
+            }}<br />{{ inApp.questions.text[2] }}
+          </p>
+          <p v-if="cutType === 4" class="text">
+            {{ inApp.subjectiveQuestion }}
+          </p>
         </div>
         <img src="/_nuxt/static/images/mockup.png" alt="" class="mockup" />
       </div>
@@ -27,6 +35,7 @@ export default {
       s: 0,
       c: 0,
       t: 0,
+      cutType: '',
       initBtn: '',
       nextBtn: '',
       inApp: {
@@ -77,17 +86,32 @@ export default {
     this.game = appData
     // 첫 데이터 찾기
 
-    this.initBtn =
-      this.game.scenarioList[this.s].chapters[this.c].initBtn[this.t]
-
-    this.inApp =
-      this.game.scenarioList[this.s].chapters[this.c].cuts[this.initBtn].list
+    this.updateGame()
   },
   methods: {
     nextGame() {
       this.t++
+      // console.log(this.game.scenarioList[this.s].chapters[this.c].length)
       // 컷 구분
-
+      console.log(this.s, this.c, this.t)
+      if (
+        this.game.scenarioList[this.s].chapters[this.c].initBtn.length ===
+        this.t
+      ) {
+        console.log('챕터 끝')
+        this.t = 0
+        this.c++
+        console.log('chpter length', this.initBtn)
+        if (this.game.scenarioList[this.s].chapters.length === this.c) {
+          this.t = 0
+          this.c = 0
+          this.s++
+          console.log('시나리오 끝')
+          if (this.game.scenarioList.length === this.s) {
+            return console.log('ending')
+          }
+        }
+      }
       this.updateGame()
     },
     updateGame() {
@@ -96,6 +120,20 @@ export default {
 
       this.inApp =
         this.game.scenarioList[this.s].chapters[this.c].cuts[this.initBtn].list
+
+      this.cutType = this.inApp.cutType
+      if (this.inApp.cutType === 1) {
+        console.log('대사')
+      }
+      if (this.inApp.cutType === 2) {
+        console.log('나레이션')
+      }
+      if (this.inApp.cutType === 3) {
+        console.log('객관식')
+      }
+      if (this.inApp.cutType === 4) {
+        console.log('주관식')
+      }
     },
   },
 }
