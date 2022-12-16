c68deecf464305c0b172617c4c1aabf78f192fe2
이채은 <ce_21126@naver.com>
2022-12-16 17:05:43 +0900

    다이얼로그

--- components/simulation/AssetsList.vue
+++ components/simulation/AssetsList.vue
@@ -2,8 +2,22 @@
   <div class="maker-left">
     <ul class="scenario-list">
       <li class="scenario-list--item active">
-        <span class="scenario-tit"> UI설정 </span>
+        <span class="scenario-tit"> 에셋관리 </span>
         <!-- {{ scen.chapters }} -->
+        <ul class="asset-list">
+          <li class="asset-list--item">
+            <span class="tit">인물</span>
+            <span class="num">5</span>
+          </li>
+          <li class="asset-list--item">
+            <span class="tit">배경</span>
+            <span class="num">15</span>
+          </li>
+          <li class="asset-list--item">
+            <span class="tit">사운드</span>
+            <span class="num">1,000</span>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
--- components/simulation/cut/DialogueSetting.vue
+++ components/simulation/cut/DialogueSetting.vue
@@ -64,6 +64,33 @@
         <div slot="button-prev" class="swiper-button-prev"></div>
         <div slot="button-next" class="swiper-button-next"></div>
       </swiper>
+      <div v-if="cutType === 1 || cutType === 2" class="insert-wrap">
+        <textarea rows="3" placeholder="대사를 입력해 주세요. 대사는 세 줄까지만 입력 가능합니다."></textarea>
+        <div class="insert-set">
+          <button v-show="!pointSettingShow && !scenarioSettingShow" type="button" class="btn point" @click="onClickPointSetting">포인트 조건</button>
+          <div v-show="pointSettingShow" class="set point-set">
+            <select class="input-select">
+              <option></option>
+              <option></option>
+            </select>
+            <input type="number" class="input-number" min="1" max="100" />
+            <span class="text">포인트</span>
+            <select class="input-select">
+              <option></option>
+              <option></option>
+            </select>
+            <button type="button" class="save" @click="onClickPointSetting('save')">닫기</button>
+          </div>
+          <button v-show="!pointSettingShow && !scenarioSettingShow" type="button" class="btn scenario" @click="onClickScenarioSetting">시나리오 연결</button>
+          <div v-show="scenarioSettingShow" class="set scenario-set">
+            <select class="input-select">
+              <option></option>
+              <option></option>
+            </select>
+            <button type="button" class="save" @click="onClickScenarioSetting('save')">닫기</button>
+          </div>
+        </div>
+      </div>
       <div v-if="cutType === 3 || cutType === 4" class="timer">
         <button type="button" class="btn set">타이머 설정</button>
         <div class="btn-wrap">
@@ -190,17 +217,13 @@
             </div>
           </div>
         </div>
-        <label v-if="cutType === 4" class="label">질문</label>
+        <!-- <label v-if="cutType === 4" class="label">질문</label>
         <div v-if="cutType === 4" class="insert-wrap">
           <textarea
             rows="3"
             :value="PREVIEW.data.subjectiveQuestion"
             @input="onInputDataSubjectiveQuestion"
           ></textarea>
-          <!-- <div class="insert-set">
-            <button type="button" class="btn sound">사운드 설정</button>
-            <div class="set sound-set"></div>
-          </div> -->
         </div>
         <div v-if="cutType === 3" class="insert-wrap">
           <input
@@ -210,8 +233,6 @@
             :value="PREVIEW.data.questions.text[0]"
             @input="onInputDataQuestions(0, $event)"
           />
-          <!-- 객관식 버튼 1 -->
-          <!-- temp1.nextElementSibling.classList.toggle('aaa') -->
           <div class="insert-set">
             <button
               v-show="!pointSettingShow"
@@ -306,7 +327,6 @@
             :value="PREVIEW.data.questions.text[1]"
             @input="onInputDataQuestions(1, $event)"
           />
-          <!-- 객관식 버튼 2 -->
           <div class="insert-set">
             <button
               v-show="!pointSettingShow"
@@ -401,7 +421,6 @@
             :value="PREVIEW.data.questions.text[2]"
             @input="onInputDataQuestions(2, $event)"
           />
-          <!-- 객관식 버튼 3 -->
           <div class="insert-set">
             <button
               v-show="!pointSettingShow"
@@ -563,7 +582,7 @@
                 닫기
               </button>
             </div>
-            <!-- <button
+            <button
             v-show="(!pointSettingShow && cutType !== 1)"
             type="button"
             class="btn point"
@@ -609,7 +628,7 @@
             >
               닫기
             </button>
-          </div> -->
+          </div>
             <button
               v-show="!scenarioSettingShow && detailSettingButton"
               type="button"
@@ -619,7 +638,7 @@
               시나리오 연결
             </button>
             <div v-show="scenarioSettingShow" class="set scenario-set">
-              <!-- <select v-if="SCENE_DATA" class="input-select">
+              <select v-if="SCENE_DATA" class="input-select">
                 <option
                   v-for="(v, i) in SCENE_DATA"
                   :key="i"
@@ -635,7 +654,6 @@
                 <option>챕터2</option>
                 <option>챕터2</option>
               </select>
-              eventCut-->
               <select
                 v-if="CUT_LIST && CUT_LIST.jsonData"
                 class="input-select"
@@ -659,8 +677,88 @@
               </button>
             </div>
           </div>
-        </div>
-        <!-- <button v-if="cutType === 3" type="button" class="cut-add"></button> -->
+        </div> -->
+      </div>
+      <div v-if="cutType === 4">
+        <label class="label">질문</label>
+        <label class="label">답변</label>
+          <div class="insert-answer">
+            <input type="text" class="input-text" />
+            <button
+              type="button"
+              class="btn link"
+              :class="{ active: isActiveEventLink }"
+              @click="onClickIsActiveEventLink"
+            ></button>
+            <div v-show="isActiveEventLink" class="event-con">
+              <div class="tab-list sm">
+                <button
+                  id="isActivePointTab4"
+                  type="button"
+                  class="tab-list--btn active"
+                  @click="onClickTabActive(4, $event)"
+                >
+                  포인트 설정
+                </button>
+                <button
+                  id="isActiveScenarioTab4"
+                  type="button"
+                  class="tab-list--btn"
+                  @click="onClickTabActive(4, $event)"
+                >
+                  시나리오 연결
+                </button>
+              </div>
+              <div id="pointTab4" class="set point-set active">
+                <select
+                  v-if="SCENE_DATA_CHARACTER && SCENE_DATA_CHARACTER.jsonData"
+                  class="input-select"
+                  :value="PREVIEW.data.questionsPoint[0].pointCr"
+                  @change="dataPointUpdate('pointCr0', $event)"
+                >
+                  <option :value="null">선택안함</option>
+                  <option
+                    v-for="(v, i) in SCENE_DATA_CHARACTER.jsonData"
+                    :key="'SCENE_DATA_CHARACTER' + i"
+                    :value="v.name"
+                  >
+                    {{ v.name }}
+                  </option>
+                </select>
+                <select v-else disabled class="input-select">
+                  <option>캐릭터 없음</option>
+                </select>
+                <input type="number" class="input-number" min="1" max="100" />
+                <span class="text">포인트</span>
+                <select
+                  class="input-select"
+                  :value="PREVIEW.data.questionsPoint[1].pointType"
+                  @change="dataPointUpdate('pointType1', $event)"
+                >
+                  <option :value="null">선택</option>
+                  <option value="P">증가</option>
+                  <option value="M">감소</option>
+                </select>
+              </div>
+              <div id="scenarioTab4" class="set scenario-set">
+                <select
+                  v-if="CUT_LIST && CUT_LIST.jsonData"
+                  class="input-select"
+                  :value="PREVIEW.data.questionsPoint[0].nextBtn"
+                  @change="dataPointUpdate('nextBtn0', $event)"
+                >
+                  <option :value="null">선택안함</option>
+                  <option
+                    v-for="(v, i) in CUT_LIST.jsonData"
+                    :key="CUT_LIST.idx[i]"
+                    :value="CUT_LIST.idx[i]"
+                  >
+                    {{ CUT_LIST.jsonData.length - i }}
+                  </option>
+                </select>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
     <div class="text-center">
@@ -736,8 +834,6 @@ export default {
       },
 
       cutListShow: false,
-      pointSettingShow: false,
-      scenarioSettingShow: false,
       // cutType: 1,
       rightContentShow: false,
       detailSettingButton: true,
@@ -769,6 +865,8 @@ export default {
       isActiveEventLink: false,
       isActivePointTab: true,
       isActiveScenarioTab: false,
+      pointSettingShow: false,
+      scenarioSettingShow: false,
     }
   },
   computed: {
@@ -871,25 +969,6 @@ export default {
     onCiickDataCr(e) {
       console.log(e)
       this.MUTATIONS_ASSETS_DATA_CR(e)
-    },
-
-    onClickPointSetting(type) {
-      this.pointSettingShow = true
-      this.scenarioSettingShow = false
-      this.detailSettingButton = false
-      if (type === 'save') {
-        this.pointSettingShow = false
-        this.detailSettingButton = true
-      }
-    },
-    onClickScenarioSetting(type) {
-      this.pointSettingShow = false
-      this.scenarioSettingShow = true
-      this.detailSettingButton = false
-      if (type === 'save') {
-        this.scenarioSettingShow = false
-        this.detailSettingButton = true
-      }
     },
     onClickChangeCutType(type) {
       // this.MUTATIONS_ASSETS_INIT_TEXT()
@@ -1134,17 +1213,19 @@ export default {
       }
       target.classList.toggle('active')
     },
+    onClickPointSetting(type) {
+      this.pointSettingShow = true
+      this.scenarioSettingShow = false
+      if (type === 'save') this.pointSettingShow = false
+    },
+    onClickScenarioSetting(type) {
+      this.pointSettingShow = false
+      this.scenarioSettingShow = true
+      if (type === 'save') this.scenarioSettingShow = false
+    },
   },
 }
 </script>
 
 <style lang="scss">
-.insert-answer {
-  .set {
-    display: none;
-    &.active {
-      display: block;
-    }
-  }
-}
 </style>
