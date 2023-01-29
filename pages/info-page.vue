95fefa522568827e6f53098e31bdb013996fc66e
이채은 <ce_21126@naver.com>
2023-01-29 13:50:37 +0900

    소개

--- pages/info-page.vue
+++ pages/info-page.vue
@@ -1,26 +1,173 @@
 <template>
-  <div class="main-section main-top">
-    <div class="main-top--tit">
-      PROJECT KOO
-      <p class="sub">내가 만드는 첫번째 어플리케이션</p>
+  <div>
+    <div class="header bg">
+      <div class="header-wrap">
+        <div class="left">
+          <div class="header-logo"><nuxt-link to="">PROJECT KOO<span class="maker">. MAKER</span></nuxt-link></div>
+          <ul class="header-menu">
+            <li class="header-menu--item"><nuxt-link to="">프로젝트 Koo</nuxt-link></li>
+            <li class="header-menu--item"><nuxt-link to="">공유</nuxt-link></li>
+            <li class="header-menu--item"><nuxt-link to="">이용방법</nuxt-link></li>
+            <li class="header-menu--item"><nuxt-link to="">금액</nuxt-link></li>
+          </ul>
+        </div>
+        <div class="right">
+          <nuxt-link to="" class="btn btn-login">로그인</nuxt-link>
+          <nuxt-link to="" class="btn btn-primary">회원가입</nuxt-link>
+        </div>
+      </div>
     </div>
-    <p class="main-top--sub">
-      프로젝트 Koo는 내가 상상하고 원하던 스토리를 쉽게 앱으로 제작하여 출시할 수 있습니다.<br />
-      다양한 경험을 지금 시작하세요.
-    </p>
-    ios and steam
-    <div class="main-top--shape">
-      <div class="circle-1"></div>
-      <div class="circle-2"></div>
-      <div class="circle-3"></div>
-      <div class="line"></div>
+
+    <div class="main-section main-top">
+      <div class="main-top--tit">
+        PROJECT KOO
+        <p class="sub">내가 만드는 첫번째 어플리케이션</p>
+      </div>
+      <p class="main-top--sub">
+        프로젝트 Koo는 내가 상상하고 원하던 스토리를 쉽게 앱으로 제작하여 출시할 수 있습니다.<br />
+        다양한 경험을 지금 시작하세요.
+      </p>
+      <div class="main-top--app">
+        <button class="btn type-1">IOS</button>
+        <button class="btn type-2">Android</button>
+        <button class="btn type-3">Steam<br />(지원예정)</button>
+      </div>
+      <div class="main-top--shape">
+        <div class="circle-1"></div>
+        <div class="circle-2"></div>
+        <div class="circle-3"></div>
+        <div class="line"></div>
+      </div>
     </div>
+
+    <div class="main-section">
+      <div class="main-section--left">
+        <div class="text-wrap" >
+          <div class="step">STEP 1</div>
+          <div class="tit">
+            내 마음대로, <br />
+            취향대로 꾸미는 UI
+          </div>
+          <div class="con">
+            나만의 어플리케이션을 위한 준비<br />
+            디자인 고민 없이 내가 원하는 UI 디자인을 체험하세요
+          </div>
+        </div>
+      </div>
+      <div class="main-section--right">
+        <div class="img-wrap play">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <div class="main-section">
+      <div class="main-section--left">
+        <div class="img-wrap images">
+          <img src="../static/images/main_img2_1.png" alt="" />
+          <img src="../static/images/main_img2_2.png" alt="" />
+          <img src="../static/images/main_img2_3.png" alt="" />
+        </div>
+      </div>
+      <div class="main-section--right">
+        <div class="text-wrap">
+          <div class="step">STEP 2</div>
+          <div class="tit">
+            다양한 에셋들로<br />
+            한층 더 퀄리티 UP!
+          </div>
+          <div class="con">
+            다양한 인물과 배경 에셋을 만나보세요<br />
+            과연 오늘은 누가, 어디서 주인공이 될까요?
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="main-section">
+      <div class="main-section--left">
+        <div class="text-wrap">
+          <div class="step">STEP 3</div>
+          <div class="tit">
+            스토리를 만들어가고,<br />
+            선택에 따른 엔딩까지
+          </div>
+          <div class="con">
+            내가 생각했던 대사들을 넣어 스토리를 만들어 보세요<br />
+            선택지에 따른 엔딩도 다양하게 설정할 수 있습니다.
+          </div>
+        </div>
+      </div>
+      <div class="main-section--right">
+        <div class="img-wrap">
+          <img src="../static/images/main_img3.png" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <div class="main-section">
+      <div class="main-section--center">
+        <div class="text-wrap">
+          <div class="tit">
+            세상에서 가장 쉬운<br />나만의 스토리 만들기
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="img-wrap play">
+            <img src="" alt="" />
+          </div>
+          <div class="right">
+            <button type="button" class="btn tit active" @click="onClickTitActive">1. 비주얼 노벨</button>
+            <div class="con">
+              상상만 하고 있던 매력적인 스토리,<br />
+              이제 게임으로 만들어 보세요.
+            </div>
+            <button type="button" class="btn tit" @click="onClickTitActive">2. 어린이 학습용 문제집</button>
+            <div class="con">
+              객관식, 주관식 문제 풀이로<br />
+              어린이 학습용 문제집을 비주얼 라이징 하게!
+            </div>
+            <button type="button" class="btn tit" @click="onClickTitActive">3. 지루할 틈 없는 방 탈출 게임</button>
+            <div class="con">
+              추리해서 문제를 풀고,<br />
+              루트가 복잡한 방탈출 게임도 손쉽게!
+            </div>
+            <button type="button" class="btn tit" @click="onClickTitActive">4. 우리끼리 만드는 연애편지</button>
+            <div class="con">
+              글로만 적어서 보냈던 연애편지,<br />
+              서로의 사진을 담아 조금 더 특별하게!
+            </div>
+            <button type="button" class="btn tit" @click="onClickTitActive">5. 그 밖에 다양한 스토리</button>
+            <div class="con">
+              다양한 이미지와 사운드를 넣어<br />
+              나만의 특별한 스토리 게임을 만들어 보세요.
+            </div>
+            <div class="mt-5">
+              <button type="button" class="button xlg btn-primary">지금 시작하기</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
+// import AOS from 'aos';
+// import "aos/dist/aos.css";
 
+export default {
+  created() {
+      // AOS.init();
+  },
+  methods: {
+    onClickTitActive(e){
+      console.log(e.target)
+      document.querySelector('.btn.tit.active').classList.remove('active')
+      e.target.classList.add('active')
+    }
+  },
 }
 </script>
 
