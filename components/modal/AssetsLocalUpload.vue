89e600be3bb48748e6ead9b6217372a49b87b265
장영욱 <raeben@naver.com>
2023-02-23 22:15:34 +0900

    fix

--- components/modal/AssetsLocalUpload.vue
+++ components/modal/AssetsLocalUpload.vue
@@ -95,7 +95,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, mapMutations } from 'vuex'
 import { kooLogin } from '~/config/util'
 export default {
   props: {
@@ -137,6 +137,7 @@ export default {
     console.log(this.modalVisible)
   },
   methods: {
+    ...mapMutations(['MUTATIONS_LOADING_INIT', 'MUTATIONS_LOADING']),
     beforeClose() {
       this.isOpen = false
     },
@@ -183,6 +184,7 @@ export default {
       }
     },
     onSubmit() {
+      this.MUTATIONS_LOADING_INIT()
       if (!this.params.gas_name) {
         return alert('에셋 이름을 작성해 주세요')
       }
@@ -234,6 +236,7 @@ export default {
             this.$emit('assetsInsertIsClose', false)
             this.isDisabled = false
           }
+          this.MUTATIONS_LOADING()
         })
         .catch((res) => {
           console.log('AXIOS FALSE', res)
