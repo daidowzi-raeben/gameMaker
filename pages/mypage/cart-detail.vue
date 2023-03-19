ef64ef22dcfba170845e3c6471f02695109edee1
박재영 <bahkjy@gmail.com>
2023-03-19 22:36:44 +0900

    카트 상세페이지 마크업

---
+++ pages/mypage/cart-detail.vue
@@ -0,0 +1,364 @@
+<template>
+  <div class="mypage-wrap type2 store">
+    <div class="cart">
+      <div class="cart-left">
+        <div
+          class="list list-asset el-scrollbar__wrap el-scrollbar__wrap--hidden-default"
+        >
+          <div class="item">
+            <div class="item-check">
+              <label class="input-check">
+                <input type="checkbox" checked />
+                <span class="check checkbox checkbox-round"></span>
+              </label>
+            </div>
+            <div class="item-img">
+              <img
+                src="~/static/images/mypage.png"
+                alt="구매내역 이미지 미리보기"
+              />
+            </div>
+            <div class="item-txt">
+              <div class="tit tit-cart">
+                판타지 핑뚝이 여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이
+                여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총
+                세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총 세줄까지만
+                표시됩니다
+              </div>
+              <ul class="info">
+                <li class="info-list">
+                  <label class="label">제작자</label>
+                  <span class="text">
+                    <span>쟈몽</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">구성</label>
+                  <span class="text">
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">가격</label>
+                  <span class="text"> <span>5,000</span><span>원</span> </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">할인가격</label>
+                  <span class="text"> <span>4,500</span><span>원</span> </span>
+                </li>
+              </ul>
+              <button type="button" class="button sm btn-white">삭제</button>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-check">
+              <label class="input-check">
+                <input type="checkbox" checked />
+                <span class="check checkbox checkbox-round"></span>
+              </label>
+            </div>
+            <div class="item-img">
+              <img
+                src="~/static/images/mypage.png"
+                alt="구매내역 이미지 미리보기"
+              />
+            </div>
+            <div class="item-txt">
+              <div class="tit tit-cart">
+                판타지 핑뚝이 여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이
+                여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총
+                세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총 세줄까지만
+                표시됩니다
+              </div>
+              <ul class="info">
+                <li class="info-list">
+                  <label class="label">제작자</label>
+                  <span class="text">
+                    <span>쟈몽</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">구성</label>
+                  <span class="text">
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">가격</label>
+                  <span class="text"> <span>5,000</span><span>원</span> </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">할인가격</label>
+                  <span class="text"> <span>4,500</span><span>원</span> </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-check">
+              <label class="input-check">
+                <input type="checkbox" checked />
+                <span class="check checkbox checkbox-round"></span>
+              </label>
+            </div>
+            <div class="item-img">
+              <img
+                src="~/static/images/mypage.png"
+                alt="구매내역 이미지 미리보기"
+              />
+            </div>
+            <div class="item-txt">
+              <div class="tit tit-cart">
+                판타지 핑뚝이 여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이
+                여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총
+                세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총 세줄까지만
+                표시됩니다
+              </div>
+              <ul class="info">
+                <li class="info-list">
+                  <label class="label">제작자</label>
+                  <span class="text">
+                    <span>쟈몽</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">구성</label>
+                  <span class="text">
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">가격</label>
+                  <span class="text"> <span>5,000</span><span>원</span> </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">할인가격</label>
+                  <span class="text"> <span>4,500</span><span>원</span> </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-check">
+              <label class="input-check">
+                <input type="checkbox" checked />
+                <span class="check checkbox checkbox-round"></span>
+              </label>
+            </div>
+            <div class="item-img">
+              <img
+                src="~/static/images/mypage.png"
+                alt="구매내역 이미지 미리보기"
+              />
+            </div>
+            <div class="item-txt">
+              <div class="tit tit-cart">
+                판타지 핑뚝이 여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이
+                여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총
+                세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총 세줄까지만
+                표시됩니다
+              </div>
+              <ul class="info">
+                <li class="info-list">
+                  <label class="label">제작자</label>
+                  <span class="text">
+                    <span>쟈몽</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">구성</label>
+                  <span class="text">
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">가격</label>
+                  <span class="text"> <span>5,000</span><span>원</span> </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">할인가격</label>
+                  <span class="text"> <span>4,500</span><span>원</span> </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-check">
+              <label class="input-check">
+                <input type="checkbox" checked />
+                <span class="check checkbox checkbox-round"></span>
+              </label>
+            </div>
+            <div class="item-img">
+              <img
+                src="~/static/images/mypage.png"
+                alt="구매내역 이미지 미리보기"
+              />
+            </div>
+            <div class="item-txt">
+              <div class="tit tit-cart">
+                판타지 핑뚝이 여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이
+                여자 캐릭터 총 세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총
+                세줄까지만 표시됩니다판타지 핑뚝이 여자 캐릭터 총 세줄까지만
+                표시됩니다
+              </div>
+              <ul class="info">
+                <li class="info-list">
+                  <label class="label">제작자</label>
+                  <span class="text">
+                    <span>쟈몽</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">구성</label>
+                  <span class="text">
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                    <span>무표정</span>
+                  </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">가격</label>
+                  <span class="text"> <span>5,000</span><span>원</span> </span>
+                </li>
+                <li class="info-list">
+                  <label class="label">할인가격</label>
+                  <span class="text"> <span>4,500</span><span>원</span> </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="cart-right">
+        <div
+          class="list list-asset el-scrollbar__wrap el-scrollbar__wrap--hidden-default"
+        >
+          <div class="box-buy">
+            <ol class="box-buy--list">
+              <li>
+                <div class="list-wrap">
+                  <label class="label">총 상품가격</label>
+                  <div class="detail"><span>5,000</span><span>원</span></div>
+                </div>
+                
+              </li>
+              <li>
+                <div class="list-wrap">
+                  <label class="label">할인가격</label>
+                  <div class="detail red"><span>4,500</span><span>원</span></div>
+                </div>
+              </li>
+            </ol>
+            <ol class="box-buy--list">
+              <li class="list-input">
+                <div class="list-wrap">
+                  <label class="label">적립금 사용</label>
+                  <div class="detail red"><span>0</span><span>원</span></div>
+                </div>
+
+                <input
+                  type="text"
+                  class="input-text"
+                  value="원"
+                />
+              </li>
+              <li class="list-input">
+                <div class="list-wrap">
+                  <label class="label">쿠폰 사용</label>
+                </div>
+
+                <input
+                  type="text"
+                  class="input-text"
+                  placeholder="쿠폰번호를 입력하세요"
+                />
+              </li>
+              <li class="list-input">
+                <div class="list-wrap">
+                  <label class="label">신용/체크카드</label>
+                </div>
+
+                <select
+                class="input-select"
+              >
+                <option value="">선택</option>
+                <option value="">국민카드</option>
+                <option value="">신한카드</option>
+              </select>
+              </li>
+            </ol>
+            <ol class="box-buy--list">
+              <li>
+                <div class="list-wrap">
+                  <label class="label">총 에셋</label>
+                  <div class="detail"><span class="total">2</span><span>개</span></div>
+                </div>
+                
+              </li>
+              <li>
+                <div class="list-wrap">
+                  <label class="label">총 결제금액</label>
+                  <div class="detail"><span class="total">4,500</span><span>원</span></div>
+                </div>
+              </li>
+            </ol>
+            <button type="button" class="btn-buy lg">구매하기</button>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'index',
+}
+</script>
+
+<style lang="scss"></style>
