87c78a0f32bce9e908a89aac7e4b5c9f45b0c755
Jaeyoung Park <bahkjy@gmail.com>
2023-03-24 12:13:21 +0900

    엔딩크레딧 페이지 생성

---
+++ pages/credit/ending-credit.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="credit">
+        <div class="credit-wrap">
+            <div class="list">
+                <div class="list-tit">
+                    <p>엔딩크레딧제목</p>
+                </div>
+                <ol class="list-detail">
+                    <li class="">
+                        <label>풀스택/디자인</label>
+                        <span>장영욱</span>
+                    </li>
+                    <li class="">
+                        <label>풀스택/디자인</label>
+                        <span>장영욱</span>
+                    </li>
+                    <li class="">
+                        <label>풀스택/디자인</label>
+                        <span>장영욱</span>
+                    </li>
+                    <li class="">
+                        <label>풀스택/디자인</label>
+                        <span>장영욱</span>
+                    </li>
+                    <li class="">
+                        <label>풀스택/디자인</label>
+                        <span>장영욱</span>
+                    </li>
+                    <li class="">
+                        <label>풀스택/디자인</label>
+                        <span>장영욱</span>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script></script>
+  
+<style lang="scss"></style>
+  
