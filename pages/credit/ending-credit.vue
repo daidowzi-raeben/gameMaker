39ab1aa502a9d985a81ce006d7fc447a2ec4925a
Jaeyoung Park <bahkjy@gmail.com>
2023-03-24 16:12:43 +0900

    엔딩크레딧 마크업

--- pages/credit/ending-credit.vue
+++ pages/credit/ending-credit.vue
@@ -1,42 +1,85 @@
 <template>
-    <div class="credit">
-        <div class="credit-wrap">
-            <div class="list">
-                <div class="list-tit">
-                    <p>엔딩크레딧제목</p>
-                </div>
-                <ol class="list-detail">
-                    <li class="">
-                        <label>풀스택/디자인</label>
-                        <span>장영욱</span>
-                    </li>
-                    <li class="">
-                        <label>풀스택/디자인</label>
-                        <span>장영욱</span>
-                    </li>
-                    <li class="">
-                        <label>풀스택/디자인</label>
-                        <span>장영욱</span>
-                    </li>
-                    <li class="">
-                        <label>풀스택/디자인</label>
-                        <span>장영욱</span>
-                    </li>
-                    <li class="">
-                        <label>풀스택/디자인</label>
-                        <span>장영욱</span>
-                    </li>
-                    <li class="">
-                        <label>풀스택/디자인</label>
-                        <span>장영욱</span>
-                    </li>
-                </ol>
-            </div>
+  <div class="credit">
+    <div class="credit-wrap">
+      <div class="container">
+        <div class="list">
+          <div class="list-tit">
+            <p class="game">게임이름게임이름</p>
+          </div>
         </div>
+        <div class="list">
+          <div class="list-tit">
+            <p>에셋 제작자</p>
+          </div>
+          <ol class="list-detail">
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+            <li class="list-detail asset">
+              <label>누구누구누구</label>
+            </li>
+          </ol>
+        </div>
+        <div class="list">
+          <div class="list-tit">
+            <p>게임 제작자</p>
+          </div>
+          <ol class="list-detail">
+            <li class="list-detail game">
+              <label>퍼블리싱/일러스트</label>
+              <span>홍홍홍</span>
+            </li>
+            <li class="list-detail game">
+              <label>풀스택/디자인</label>
+              <span>홍홍홍</span>
+            </li>
+            <li class="list-detail game">
+              <label>퍼블리싱 및 마케팅</label>
+              <span>홍홍홍</span>
+            </li>
+            <li class="list-detail game">
+              <label>자바</label>
+              <span>홍홍홍</span>
+            </li>
+            <li class="list-detail game">
+              <label>풀스택/디자인</label>
+              <span>홍홍홍</span>
+            </li>
+            <li class="list-detail game">
+              <label>풀스택/디자인</label>
+              <span>홍홍홍</span>
+            </li>
+          </ol>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
-  
+
 <script></script>
-  
+
 <style lang="scss"></style>
-  
